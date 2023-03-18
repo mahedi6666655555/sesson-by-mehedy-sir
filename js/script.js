165bedbbec8ed9bd1e6a09b8fb266534b4f0851d
mahedi6666655555 <mahedinir34678@gmail.com>
2023-03-19 01:10:58 +0600

    all practise is done all ok this time to go to bed

---
+++ js/script.js
@@ -0,0 +1,190 @@
+// write a dicler a variable
+
+// let mahedi this is chengable like or const is not updateable and not chengable
+
+// let mahedi=10
+//  mahedi=20
+
+//  console.log(mahedi);
+
+// wriet const and try to update the value
+
+// const mahedi=10
+
+// mahedi=100
+
+// console.log(mahedi);
+
+// how to write condition in javascript  lets begain
+
+// let mahedi=1000
+// let mahedi2=100
+// if(mahedi===1000 && mahedi2===100){
+//     console.log("mahei is greate");
+// }
+
+// let mahei=[11,2,33,44,55,66,77]
+
+// for(let i=0; i<mahei.length; i++){
+//     let element=mahei[i]
+
+//     console.log(element);
+// }
+
+// for(let maheid of mahei )
+
+// console.log(maheid);
+
+// let i=0
+
+// while(i<mahei.length){
+//     let element=mahei[i]
+//     console.log(element);
+//     i++
+// }
+
+//how to write function
+
+// function mahedi(x,y){
+
+//     let var1=x+y
+//     return r=var1;
+
+// }
+
+// let monko=mahedi(10,20)
+// console.log(monko);
+
+// let name="mahedi-hassan"
+
+// let mahedi=`
+
+// my name is ${name}
+
+// my name is ${name}
+
+// my name is ${name}
+
+// `
+
+
+// console.log(mahedi);
+
+
+
+
+// let mahei=(x,y)=>{
+//     let total=x+y
+//     console.log(total);
+// }
+
+
+// mahei(10,10)
+
+// let mahedi=x=>x*2
+
+// let total=mahedi(10)
+// console.log(total);
+
+
+// let mahei=[11,22,33,44,55,66,77]
+
+
+// let total=[...mahei]
+// total.push(20)
+// console.log(mahei,total);
+
+
+
+
+// let mahei=[11,22,33,44,5,66,77]
+
+// let [king,man,modon]=mahei
+
+// console.log(modon);
+
+
+// let info={
+
+
+//     name:"mahei",
+//     age:17,
+//     email:"maheidinir34678@gmail.com"
+    
+// }
+//  let {name,age,email}=info
+
+//  console.log(email);
+
+
+// to convert JSON
+// const jsonData = { "name": "John", "age": 22 };   
+
+// let total=JSON.stringify(jsonData)
+
+// console.log(total);
+
+
+// to convert parse
+// const jsonData = '{ "name": "John", "age": 22 }';   
+
+// let total=JSON.parse(jsonData)
+// console.log(total);
+
+
+
+
+// how to lod api and fetching
+
+
+// let loddatas=()=>{
+
+
+//     let url=``
+
+//     fetch(url)
+//     .then(res=>res.json())
+//     .then((data)=>console.log(data))
+
+// }
+
+// loddatas()
+
+// let m="mahedi"
+
+// let total=!!m
+// console.log(total);
+
+
+// let mahedi=100
+
+// let total=mahedi===10?"alex xosta is monko":"alex is jonto"
+
+// console.log(total);
+
+
+
+
+// let mahedi=10
+
+// let total=parseFloat(mahedi)
+
+// console.log(total);
+
+let mahedi={
+
+
+    name:"mahedi",
+    age:17,
+    email:"mahedinir34678@gmail.com"
+
+}
+
+// this is Dot notation 
+
+let total=mahedi.name
+console.log(total);
+
+// this is bracket notation
+// let total=mahedi["name"]
+// console.log(total);
